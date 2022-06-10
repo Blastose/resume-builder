eb65453fb8cbbb0dc4b30f503594d1f876b7ce12
Blastose <blastoisejay@gmail.com>
2022-06-10 15:22:46 -0700

    Add base experience section

--- src/components/cv-output/cv-layout.tsx
+++ src/components/cv-output/cv-layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CVPersonalInfo from "../personal-info/cv-personal-info";
+import CVEducation from "../education/cv-education";
+import CVExperience from "../work-experience/cv-experience";
 
 class CVLayout extends React.Component {
   render() {
     return (
-      <div className="bg-white h-min py-4 px-8">
+      <div className="bg-white h-min py-4 px-8 flex flex-col gap-2">
         <CVPersonalInfo />
+        <CVEducation />
+        <CVExperience />
       </div>
     );
   }
---
+++ src/components/work-experience/cv-experience.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CVExperience extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default CVExperience;
